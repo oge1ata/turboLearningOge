0130a67e09117ae76c5a094930e0430bbdcc9754
oge1ata <yomacorp54@gmail.com>
2024-02-10 20:48:35 +0100

    Redid no 3

--- hms-apps/identity/src/app.module.ts
+++ hms-apps/identity/src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      host: 'localhost',
+      database: 'hms',
+      username: 'root',
+      password: 'root',
+      synchronize: true,
+      autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false, // Reject unauthorized SSL connections
+    },
+
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {}
--- hms-apps/identity/src/users/entities/user.entity.ts
+++ hms-apps/identity/src/users/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ unique: true })
+    primaryEmailAddress: string;
+
+    @Column()
+    firstName: string;
+
+    @Column({ nullable: true })
+    middleName?: string | undefined;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    passwordHash: string;
+
+    @Column()
+    backupEmailAddress: string;
+
+    @Column({ type: "json", nullable: true })
+    // phone: { [key: string]: any } | undefined;
+    phone: { [key: string]: any } | undefined;
+
+    @Column({ default: false })
+    isPrimaryEmailAddressVerified: boolean;
+
+    @Column({ default: false })
+    isBackupEmailAddressVerified: boolean;
+}
